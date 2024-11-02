63119a186dadba06cee3509672b3f655fb7546db
GitAadil27 <cityadil024@gmail.com>
2024-11-02 20:48:22 +0300

    Update settings.py

--- CBKNEW/cbk_upload/settings.py
+++ CBKNEW/cbk_upload/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import whitenoise
+import os
 from whitenoise.middleware import WhiteNoiseMiddleware
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
